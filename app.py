9e69fd688608f22c25415b71dd8afcd9a16b0fe6
George Mihov <gmihov001@gmail.com>
2020-06-20 18:37:53 +0000

    Updates

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import random
 import math
 
 # your code here
+excuse = ''
+
+who = ['My cat ', 'My grandma ', 'My boss ', 'The neighbor ']
+what = ['chewed my shoes ', 'wrecked my car ', 'peed in my bed ', 'knocked on the door ']
+when = ['when I was trying to leave.', 'yesterday.', 'when you called.', 'when I was driving.']
+
