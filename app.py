8faceec2153c0868ccc043512a3877c0ddf3448d
George Mihov <gmihov001@gmail.com>
2020-06-20 18:41:18 +0000

    Updates

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import random
 import math
 
 # your code here
-excuse = ''
 
 who = ['My cat ', 'My grandma ', 'My boss ', 'The neighbor ']
 what = ['chewed my shoes ', 'wrecked my car ', 'peed in my bed ', 'knocked on the door ']
 when = ['when I was trying to leave.', 'yesterday.', 'when you called.', 'when I was driving.']
 
+excuse = who[random.randint(0,len(who)-1)] + what[random.randint(0,len(who)-1)] + when[random.randint(0,len(who)-1)]
+
+print(excuse)
