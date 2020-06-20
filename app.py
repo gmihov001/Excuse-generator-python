aee2cd4f8f6cc59873dbf84e1bd5443ba9c11a99
George Mihov <gmihov001@gmail.com>
2020-06-20 18:51:01 +0000

    Updates

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ who = ['My cat ', 'My grandma ', 'My boss ', 'The neighbor ']
 what = ['chewed my shoes ', 'wrecked my car ', 'peed in my bed ', 'knocked on the door ']
 when = ['when I was trying to leave.', 'yesterday.', 'when you called.', 'when I was driving.']
 
-excuse = who[random.randint(0,len(who)-1)] + what[random.randint(0,len(who)-1)] + when[random.randint(0,len(who)-1)]
+prevExcuse = None    
 
-print(excuse)
+newExcuse = who[random.randint(0,len(who)-1)] + what[random.randint(0,len(who)-1)] + when[random.randint(0,len(who)-1)]
+
+if(newExcuse != prevExcuse):
+    print(newExcuse)
+else:
+    print('Draw again')
+
+prevExcuse = newExcuse
